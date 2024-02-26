273d124d0d8f0b6d2781a47b2f8c54968bcde62d
pjimenezq <141860508+pjimenezq@users.noreply.github.com>
2024-02-26 14:36:11 -0500

    Update Punto uno.py

--- Punto uno.py
+++ Punto uno.py
@@ -6,16 +6,19 @@ def operaciones(x:float, y:float, operacion:str)->float:
     elif operacion == "-":
         return x-y
     elif operacion == "*":
-            return x*y
+        return x*y
     elif operacion == "/":
+        if y != 0:
             return x/y
+        else:
+            return "No se puede dividir entre 0"
     else:
-          return "El caracter que corresponde a la operación no es válido."
+        return "El caracter que corresponde a la operación no es válido."
     
 if __name__=="__main__":
-      print("Este programa realiza operaciones básicas (suma, resta, multiplicación y división) entre dos números.")
-      a = float(input("Ingrese el primer operando "))
-      b = float(input("Ingrese el segundo operando "))
-      operador = str(input("Ingrese el caracter que corresponde a la operación que desea realizar "))
-      resultado = operaciones(a, b, operador)
-      print(resultado)
+    print("Este programa realiza operaciones básicas (suma, resta, multiplicación y división) entre dos números.")
+    a = float(input("Ingrese el primer operando "))
+    b = float(input("Ingrese el segundo operando "))
+    operador = str(input("Ingrese el caracter que corresponde a la operación que desea realizar "))
+    resultado = operaciones(a, b, operador)
+    print(resultado)
